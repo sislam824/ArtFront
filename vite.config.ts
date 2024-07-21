883c8672f2cb0d8fc79f284d3fd65a2e41606c2a
Abu Osama <s.islam20000@gmail.com>
2024-07-21 11:45:18 +0530

    ccc

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+// Convert import.meta.url to __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
